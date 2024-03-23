46b079ed35c07fd477d30f71c9c2ae543efc9ad2
Ahmed Gamal <63426707+AhmedGamal-1@users.noreply.github.com>
2024-03-23 06:15:13 +0200

    created files for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import ProductDetails from './pages/ProductDetails';
-import Favorites from './pages/Favorites';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
+import Favorites from './pages/Favorites/Favorites';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Products from './pages/Products/Products';
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import ProductCard from '../components/Card/Card';
+import ProductCard from '../../components/Card/Card';
 
 function Home() {
     return (
