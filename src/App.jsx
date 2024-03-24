44b16c4a1a4d54390c62451a3d50041a079e532d
mostafataha177 <mostafataha99288@gmail.com>
2024-03-25 01:07:09 +0200

    Payment && Redux

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,34 @@ import Favorites from './pages/Favorites/Favorites';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Products from './pages/Products/Products';
 import { FavoritesProvider } from './context/FavoritesContext';
+// import { PayPalButton } from 'react-paypal-button-v2';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import Checkout from './components/Payment/Checkout';
+
+const initialOptions = {
+    'client-id':
+        'AXZreM1Xm9aIi3wL43tzzmwSoAIL2vB7OVx6Yz__unl9949iCe6SwDw7PWV-Ql2YCadYyFNEfjeuAG3g',
+    currency: 'USD',
+    intent: 'capture',
+};
 
 function App() {
     return (
-        <FavoritesProvider>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/products/:id" element={<ProductDetails />} />
-                <Route path="/favorites" element={<Favorites />} />
-            </Routes>
-        </FavoritesProvider>
+        <>
+            <PayPalScriptProvider options={initialOptions}>
+                <Checkout />
+            </PayPalScriptProvider>
+            <FavoritesProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/products/:id" element={<ProductDetails />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </FavoritesProvider>
+        </>
     );
 }
 
