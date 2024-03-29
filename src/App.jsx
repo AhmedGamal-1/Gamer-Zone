7f67c8fe43671a24e9f65d3a225720487e395bae
mostafataha177 <mostafataha99288@gmail.com>
2024-03-29 13:51:04 +0200

    redux store

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,34 @@ import Home from './pages/Home/Home';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
 import Favorites from './pages/Favorites/Favorites';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
 import Products from './pages/Products/Products';
 import Paypal from './components/Payment/Paypal';
 import { FavoritesProvider } from './context/FavoritesContext';
-// import { useState } from 'react';
-// import Checkout from './components/Payment/Paypal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import Store from '../src/redux/store/Store';
 
 function App() {
     return (
-        <>
-            <FavoritesProvider>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    {/* <Route path="/signup" element={<signup} /> */}
-                    {/* <Route path="/signin" element={}/> */}
-                    <Route path="/products" element={<Products />} />
-                    <Route path="/products/:id" element={<ProductDetails />} />
-                    <Route path="/favorites" element={<Favorites />} />
-                    <Route path="/checkout" element={<Paypal />} />
-                </Routes>
-            </FavoritesProvider>
-        </>
+        <div>
+            <Provider store={Store}>
+                <FavoritesProvider>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/signup" element={<signup></signup>} />
+                        <Route path="/signin" element={<signIn></signIn>} />
+                        <Route path="/products" element={<Products />} />
+                        <Route
+                            path="/products/:id"
+                            element={<ProductDetails />}
+                        />
+                        <Route path="/favorites" element={<Favorites />} />
+                        <Route path="/checkout/:id" element={<Paypal />} />
+                    </Routes>
+                </FavoritesProvider>
+            </Provider>
+        </div>
     );
 }
 
